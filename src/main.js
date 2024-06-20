24e616b6e6ed56310f99d77dac68d76ba02942a3
yuiyideyui <yuiyide@gmail.com>
2024-06-20 02:04:34 +0000

    save

--- src/main.js
+++ src/main.js
@@ -7,6 +7,19 @@
 import {loginBilibili} from './login.js'
 import {updateToken} from './updateToken.js'
 export default {
+/**
+ * 
+ * @param {path} cookiePath 要保存的cookie路径-JSON格式
+ * @param {path} localStoragePath 要保存的localStorage路径-JSON格式
+ * @returns 
+ */
+
     loginBilibili,
+/**
+ * 
+ * @param {path} cookiePath 账号cookie的地址
+ * @param {path} localStoragePath 账号localStoragePath的地址
+ * @returns 
+ */
     updateToken
 };
